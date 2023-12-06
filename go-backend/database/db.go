bd422008736c4fd89477c055e8a5f61c47bfe33a
GustavoSept <GustavoSept.Cron@gmail.com>
2023-12-06 18:54:26 -0300

    adding connection retry logic

--- go-backend/database/db.go
+++ go-backend/database/db.go
@@ -2,18 +2,29 @@ package database
 
 import (
 	"log"
+	"time"
 
 	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq" // PostgreSQL driver
+	_ "github.com/lib/pq"
 )
 
 var DB *sqlx.DB
 
 func ConnectToDatabase() {
-	databaseURL := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
 	var err error
-	DB, err = sqlx.Connect("postgres", databaseURL)
-	if err != nil {
-		log.Panic("Error connecting to the database:", err)
+	databaseURL := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+
+	// Retry logic
+	for i := 1; i <= 40; i++ {
+		DB, err = sqlx.Connect("postgres", databaseURL)
+		if err == nil {
+			log.Println("Database connection established")
+			return
+		}
+
+		log.Printf("Attempt %d: Database connection failed, retrying in %d milliseconds...\n", i, i*200)
+		time.Sleep(time.Duration(i*200) * time.Millisecond)
 	}
+
+	log.Panic("Error connecting to the database:", err)
 }
