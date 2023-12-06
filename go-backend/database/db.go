e3a528b7bd0353546050888c61216a4bef09d750
GustavoSept <GustavoSept.Cron@gmail.com>
2023-12-06 17:14:12 -0300

    adding database connection

---
+++ go-backend/database/db.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // PostgreSQL driver
+)
+
+var DB *sqlx.DB
+
+func ConnectToDatabase() {
+	databaseURL := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	var err error
+	DB, err = sqlx.Connect("postgres", databaseURL)
+	if err != nil {
+		log.Panic("Error connecting to the database:", err)
+	}
+}
--- go-backend/main.go
+++ go-backend/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"go-backend/database"
 	"go-backend/routes"
 )
 
 func main() {
+	database.ConnectToDatabase()
 	routes.HandleRequest()
 }
