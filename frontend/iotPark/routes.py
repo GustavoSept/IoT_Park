3bb5810193c982d34175853cd7a72a4afb1eedce
GustavoSept <GustavoSept.Cron@gmail.com>
2023-12-27 17:04:23 -0300

    testing connection

--- frontend/iotPark/routes.py
+++ frontend/iotPark/routes.py
@@ -1,6 +1,18 @@
 from iotPark import app
 from flask import render_template
+import requests
 
 @app.route('/')
-def pg_home():
-    return render_template('home.html')
+def pg_home():    
+    url = 'http://go-backend:8080/'
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return render_template('home.html', users=response.json())
+        else:
+            return f'Error: {response.status_code}'
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to backend: {e}")
+        return "Error connecting to backend."
