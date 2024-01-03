514aa708574ade26e605ea23bd477892c73a12bf
GustavoSept <GustavoSept.Cron@gmail.com>
2024-01-03 17:02:30 -0300

    improving initialization rules added multiple indices changed some UNIQUE rules

--- go-backend/migration/docker-database-initial.sql
+++ go-backend/migration/docker-database-initial.sql
@@ -1,3 +1,5 @@
+CREATE TYPE user_office_level AS ENUM ('operador', 'lavador', 'vendedor', 'dono', 'gerente');
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name VARCHAR(20) NOT NULL,
@@ -5,26 +7,32 @@ CREATE TABLE users (
     office_level user_office_level NOT NULL
 );
 
+CREATE INDEX idx_first_name ON users(first_name);
+CREATE INDEX idx_last_name ON users(last_name);
+
+
 -- ---------------------------------------------------
-
-CREATE TYPE user_office_level AS ENUM ('operador', 'lavador', 'vendedor', 'dono', 'gerente');
-
 
 CREATE TABLE parking_lots (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     addr_street VARCHAR(80) NOT NULL,
     addr_number SMALLINT NOT NULL,
-    cep VARCHAR(8) NOT NULL,
-    owner_id UUID REFERENCES users(id) UNIQUE
+    cep VARCHAR(9) NOT NULL,
+    owner_id UUID REFERENCES users(id) NOT NULL
 );
 
+CREATE INDEX idx_owner_id ON parking_lots(owner_id);
 -- ---------------------------------------------------
 
-CREATE TABLE parking_lot_employees (    
+-- Stores relations of all parking lots to users that are not of type 'dono'
+CREATE TABLE parking_lot_employees (
     user_id UUID REFERENCES users(id) NOT NULL UNIQUE,
     parking_lot_id UUID REFERENCES parking_lots(id) NOT NULL
 
 );
+
+CREATE INDEX idx_user_id ON parking_lot_employees(user_id);
+CREATE INDEX idx_parkinglot_id ON parking_lot_employees(parking_lot_id);
 
 -- ---------------------------------------------------
 CREATE TYPE car_model AS ENUM ('sedã', 'hatch', 'suv', 'cupê', 'conversível', 'picape', 'minivan', 'caminhão');
@@ -37,14 +45,12 @@ CREATE TABLE cars (
     model car_model NOT NULL,
     color car_color,
     brand car_brand,
-    license_plate VARCHAR(7) NOT NULL,
-
-    CONSTRAINT uq_license_plate UNIQUE (license_plate)
+    license_plate VARCHAR(7) NOT NULL UNIQUE
 );
 
--- ---------------------------------------------------
+CREATE INDEX idx_license_plate ON cars(license_plate);
 
-CREATE TYPE op_type AS ENUM ('entry_in', 'entry_out', 'service', 'store');
+-- ---------------------------------------------------
 
 CREATE TYPE pay_methods AS ENUM ('cartão de crédito', 'cartão de débito', 'dinheiro em espécie', 'pix');
 
@@ -61,6 +67,10 @@ CREATE TABLE entries_fiscal (
     nfe_number SMALLINT NOT NULL,
     cust_payment_id UUID REFERENCES customer_payments(id) NOT NULL UNIQUE
 );
+
+CREATE INDEX idx_nfe ON entries_fiscal(nfe_number);
+
+CREATE TYPE op_type AS ENUM ('entry_in', 'entry_out', 'service', 'store');
 
 CREATE TABLE entries_log (
     id BIGSERIAL PRIMARY KEY,
@@ -79,6 +89,10 @@ CREATE TABLE entries_log (
             OR (operation_type = 'store')
         )
 );
+
+CREATE INDEX idx_license_plate_log_non_null ON entries_log(car_license_plate)
+    WHERE car_license_plate IS NOT NULL;
+
 
 
 
