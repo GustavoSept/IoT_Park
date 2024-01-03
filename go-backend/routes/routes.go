c6c74f861acb3d18b2d195c149989110114bdc72
GustavoSept <GustavoSept.Cron@gmail.com>
2024-01-03 17:03:37 -0300

    added CORS config

--- go-backend/routes/routes.go
+++ go-backend/routes/routes.go
@@ -3,11 +3,18 @@ package routes
 import (
 	"go-backend/controllers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func HandleRequest() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://0.0.0.0:5000", "http://localhost:5000", "http://flask-frontend:5000"},
+		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "X-Requested-With", "Accept", "HX-Request", "HX-Current-URL"},
+		AllowCredentials: true,
+	}))
 
 	// Define routes
 	router.GET("/api/get_all_users", controllers.GetAllUsers)
