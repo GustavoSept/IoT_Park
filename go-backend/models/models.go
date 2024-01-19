6cb3f59eab0d23a127fc08b8c948dfbd5aa59a0e
GustavoSept <GustavoSept.Cron@gmail.com>
2024-01-19 12:04:58 -0300

    changing model tags

--- go-backend/models/models.go
+++ go-backend/models/models.go
@@ -17,6 +17,7 @@ func init() {
 	Validate.RegisterValidation("onlyNames", validation_names)
 }
 
+// onlyNames custom tag
 func validation_names(fl validator.FieldLevel) bool {
 	// accept any string that doesn't contain numbers
 	// also blocks common escape characters
@@ -26,10 +27,10 @@ func validation_names(fl validator.FieldLevel) bool {
 }
 
 type User struct {
-	ID           uuid.UUID `form:"user_id"`
-	First_Name   string    `form:"user_f_name" validate:"required,onlyNames,max=20"`
-	Last_Name    string    `form:"user_l_name" validate:"required,onlyNames,max=60"`
-	Office_Level string    `form:"user_oflvl" validate:"required,oneof=operador lavador vendedor dono gerente"`
+	ID           uuid.UUID `form:"user_id" json:"user_id"`
+	First_Name   string    `form:"user_f_name" json:"user_f_name" validate:"required,onlyNames,max=20"`
+	Last_Name    string    `form:"user_l_name" json:"user_l_name" validate:"required,onlyNames,max=60"`
+	Office_Level string    `form:"user_oflvl" json:"user_oflvl" validate:"required,oneof=operador lavador vendedor dono gerente"`
 }
 
 type User_Auth struct {
@@ -40,18 +41,18 @@ type User_Auth struct {
 }
 
 type ParkingLot struct {
-	ID         uuid.UUID `db:"id" form:"pl_id" json:"pl_id"`
-	PLot_Name  string    `db:"pl_name" form:"pl_name" json:"pl_name"`
-	AddrStreet string    `db:"addr_street" form:"pl_adstr" json:"pl_adstr" validate:"required,onlyNames,max=80"`
-	AddrNumber int       `db:"addr_number" form:"pl_adnum" json:"pl_adnum" validate:"required,numeric,min=0,max=32767"`
-	CEP        string    `db:"cep" form:"pl_cep" json:"cep" validate:"required"`
-	OwnerID    uuid.UUID `db:"owner_id" form:"owner_id" json:"owner_id"`
+	ID         uuid.UUID `db:"id" form:"pl_id"`
+	PLot_Name  string    `db:"pl_name" form:"pl_name"`
+	AddrStreet string    `db:"addr_street" form:"pl_adstr" validate:"required,onlyNames,max=80"`
+	AddrNumber int       `db:"addr_number" form:"pl_adnum" validate:"required,numeric,min=0,max=32767"`
+	CEP        string    `db:"cep" form:"pl_cep" validate:"required"`
+	OwnerID    uuid.UUID `db:"owner_id" form:"owner_id"`
 }
 
 // This struct is a sub-set of User, but when checking only an owner name
 type Owner_onlyName struct {
-	First_Name string `json:"owner_first_name" form:"owner_first_name" validate:"required,onlyNames,max=20"`
-	Last_Name  string `json:"owner_last_name" form:"owner_last_name" validate:"required,onlyNames,max=60"`
+	First_Name string `form:"owner_f_name" json:"owner_f_name" validate:"required,onlyNames,max=20"`
+	Last_Name  string `form:"owner_l_name" json:"owner_l_name" validate:"required,onlyNames,max=60"`
 }
 
 var Users []User
