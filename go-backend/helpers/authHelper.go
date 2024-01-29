947d49000fa351c178bcbea7474caec209f4fa4d
GustavoSept <GustavoSept.Cron@gmail.com>
2024-01-29 10:32:35 -0300

    helper function: MatchLoginDataWithUser

--- go-backend/helpers/authHelper.go
+++ go-backend/helpers/authHelper.go
@@ -3,7 +3,12 @@ package helpers
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
+	"go-backend/database"
+	"go-backend/models"
+	"log"
 
+	"github.com/google/uuid"
 	"golang.org/x/crypto/argon2"
 )
 
@@ -29,8 +34,8 @@ func HashPassword(password string, salt []byte) (string, error) {
 	return base64.RawStdEncoding.EncodeToString(hash), nil
 }
 
-func CheckPassword(providedPassword string, storedHash string, salt []byte) (bool, error) {
-	hash, err := HashPassword(providedPassword, salt)
+func CheckPassword(inPassword string, storedHash string, salt []byte) (bool, error) {
+	hash, err := HashPassword(inPassword, salt)
 	if err != nil {
 		return false, err // Error while hashing
 	}
@@ -39,4 +44,23 @@ func CheckPassword(providedPassword string, storedHash string, salt []byte) (boo
 	match := hash == storedHash
 
 	return match, nil
+}
+
+func MatchLoginDataWithUser(inPassword, inEmail string) (bool, error) {
+	var valUser models.User_Auth
+
+	if err := database.DB.Get(&valUser, `
+            SELECT user_id, password_hash, salt
+            FROM users_authentication
+            WHERE email=$1`, inEmail); err != nil {
+		// Log the error for internal tracking
+		log.Printf("Error fetching user data: %v", err)
+		return false, errors.New("internal server error")
+	}
+
+	if valUser.UserID == uuid.Nil {
+		return false, errors.New("Login Inválido")
+	}
+
+	return CheckPassword(inPassword, valUser.PasswordHash, valUser.Salt)
 }
