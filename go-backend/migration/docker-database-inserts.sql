9f38f403680d6cf16eff824e2f471e88a853fad1
GustavoSept <GustavoSept.Cron@gmail.com>
2024-01-29 11:26:05 -0300

    updating initial user accounts

--- go-backend/migration/docker-database-inserts.sql
+++ go-backend/migration/docker-database-inserts.sql
@@ -11,11 +11,11 @@ INSERT INTO users (id, first_name, last_name, office_level) VALUES
 ('9b1153d4-02ba-494a-9327-89600e1a00b1','Maria', 'Ferreira', 'gerente');
 
 INSERT INTO users_authentication (user_id, email, password_hash, salt) VALUES -- all passwords are `testPassword`
-('59721a63-9b2c-4754-8937-316f8a5942b3','gustLim@gmail.com', 'pVOFiaTlsSs+VwT/kwih1UpGYtPE3ihDQ0eJeyAR77Q', '0661798c29e62f313fcc20267a20cb6b'),
-('9b1153d4-02ba-494a-9327-89600e1a00b1','abobrinha@gmail.com', 'pVOFiaTlsSs+VwT/kwih1UpGYtPE3ihDQ0eJeyAR77Q', '0661798c29e62f313fcc20267a20cb6b'),
-('11111111-1111-1111-1111-111111111111','admin@gmail.com', 'pVOFiaTlsSs+VwT/kwih1UpGYtPE3ihDQ0eJeyAR77Q', '0661798c29e62f313fcc20267a20cb6b'),
-('22222222-2222-2222-2222-222222222222','Pathy+app@gmail.com', 'pVOFiaTlsSs+VwT/kwih1UpGYtPE3ihDQ0eJeyAR77Q', '0661798c29e62f313fcc20267a20cb6b'),
-('33333333-3333-3333-3333-333333333333','Roger9000@hotmail.com', 'pVOFiaTlsSs+VwT/kwih1UpGYtPE3ihDQ0eJeyAR77Q', '0661798c29e62f313fcc20267a20cb6b');
+('59721a63-9b2c-4754-8937-316f8a5942b3','gustLim@gmail.com', '2vAqglwoBay7pEHne5WFW6Kio5/uVJA0CqJtN7lbcew', '0661798c29e62f313fcc20267a20cb6b'),
+('9b1153d4-02ba-494a-9327-89600e1a00b1','abobrinha@gmail.com', 'hA1+B91+vhIawdiydhqr/bf+XyHG/ph72r5h0XF6UZw', '27103a4a063a5ad67f9652ec3d725f2c'),
+('11111111-1111-1111-1111-111111111111','adm@admin.com', 'PMdqYn1t++gXg50MXlgwEjm8bxbFbVQdXaB2TBDXVLU', '15653de26d13ea6861651915ba4ae2c6'), -- pass is: Adm1098!
+('22222222-2222-2222-2222-222222222222','Pathy+app@gmail.com', 'aZ/S6dt1tzBHhPEADbcRvUI6pqOk4p10KrXO94q5Y0E', 'a8fc781214146125518847fdb3720fc1'),
+('33333333-3333-3333-3333-333333333333','Roger9000@hotmail.com', '434+TvM1iZW6o7hWRd1aPR3uxW8liSobh1Dzz33HDQk', '49406bc22617e929a37463995d0fa7de');
 
 
 INSERT INTO parking_lots (pl_name, addr_street, addr_number, cep, owner_id) VALUES
